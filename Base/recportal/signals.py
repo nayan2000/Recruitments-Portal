03718f048e2c091a61b822e7fb122db84812d4f3
Hemanth V. Alluri <hdrive1999@gmail.com>
2018-06-29 10:39:09 +0530

    Fixed the fix to issue #8

--- Base/recportal/signals.py
+++ Base/recportal/signals.py
@@ -1,4 +1,8 @@
 from recportal.models import *
 
 def autoAddSeniorProfile(instance, **kwargs):
+    try:
+        instance.senior
+    except:
+        print("Creating a default user profile. Update later.")
         Senior.objects.create(user=instance, team="None", seniority_level=1)
