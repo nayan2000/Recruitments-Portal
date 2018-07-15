3cf668e72a66969b327bc9f4ac093557066ec96f
Hemanth V. Alluri <hdrive1999@gmail.com>
2018-07-15 23:17:25 +0530

    urls updated for the name change from pitch to assessment

--- Base/recportal/urls.py
+++ Base/recportal/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     url(r'^my-candidates/$', views.MyCandidates, name='mycandidates'),
     url(r'^candidates/$', views.Candidates, name='candidates'),
     url(r'^profile/(?P<first_name>[A-Za-z_]+)-(?P<last_name>[A-Za-z]+)/$', views.CandidateProfile, name='profile'),
-    url(r'^pitch/(?P<first_name>[A-Za-z_]+)-(?P<last_name>[A-Za-z]+)/$', views.PitchCandidate, name='pitch'),
+    url(r'^assess/(?P<first_name>[A-Za-z_]+)-(?P<last_name>[A-Za-z]+)/$', views.AssessCandidate, name='assess'),
     url(r'^recommend/(?P<first_name>[A-Za-z_]+)-(?P<last_name>[A-Za-z]+)/$', views.RecommendCandidate, name='recommend'),
-    url(r'^my-pitches/$', views.MyPitches, name="mypitches"),
-    url(r'^pitches/$', views.Pitches, name="pitches"),
+    url(r'^my-assessments/$', views.MyAssessments, name="myassessments"),
+    url(r'^assessments/$', views.Assessments, name="assessments"),
     url(r'^download/(?P<filename>.+)/$', views.Download, name="download"),
     url(r'^edit/(?P<first_name>[A-Za-z_]+)-(?P<last_name>[A-Za-z]+)/$', views.EditCandidate, name='edit')
 ]
